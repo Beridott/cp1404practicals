a7053e05b269f1a40c0da56280df3a88ab047f5d
Junyi Fan <junyi.fan@my.jcu.edu.au>
2026-05-28 19:41:12 +0800

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,9 +1,32 @@
-MIN_LENGTH = 8  # 设定的最小密码长度
+MIN_LENGTH = 8
 
-password = input("Enter password: ")
+def main():
+    """This function will coordinate the password verification and printing."""
 
-while len(password) < MIN_LENGTH:
-    print("Password too short!")
+    password = get_password(MIN_LENGTH)
+    receive_password(password)
+
+
+def receive_password(password: str):
+    """This function will print asterisks matching the length of the password."""
+    print("*" * len(password))
+
+
+def get_password(min_length: int) -> str:
+    """This function will get and validate a password of a minimum length."""
+
     password = input("Enter password: ")
 
-print("*" * len(password))
+    while len(password) < min_length:
+        print("Password too short!")
+        password = input("Enter password: ")
+    return password
+
+main()
+
+
+
+
+
+
+
