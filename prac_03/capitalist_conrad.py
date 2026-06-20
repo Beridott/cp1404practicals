8589551579e3e9254be72b68ce59cdc501d68e0a
Junyi Fan <junyi.fan@my.jcu.edu.au>
2026-06-20 14:38:40 +0800

    completed capitalist_conrad.py

---
+++ prac_03/capitalist_conrad.py
@@ -0,0 +1,33 @@
+import random
+
+INITIAL_PRICE = 10.0
+MIN_PRICE = 1.0
+MAX_PRICE = 100.0
+MAX_INCREASE = 0.175
+MAX_DECREASE = 0.05
+FILENAME = "stock_output.txt"
+
+
+price = INITIAL_PRICE
+number_of_days = 0  #
+
+
+out_file = open(FILENAME, 'w')
+
+print(f"Starting price: ${price:,.2f}", file=out_file)
+
+while MIN_PRICE <= price <= MAX_PRICE:
+    number_of_days += 1
+
+    if random.randint(1, 2) == 1:
+        price_change_percent = random.uniform(0, MAX_INCREASE)
+        price *= (1 + price_change_percent)
+    else:
+        price_change_percent = random.uniform(0, MAX_DECREASE)
+        price *= (1 - price_change_percent)
+
+    print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
+
+out_file.close()
+
+print(f"End up! {FILENAME} FILE。")
