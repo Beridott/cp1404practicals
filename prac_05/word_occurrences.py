3e1458960e64c3e49ef831e51a2e1876e3133e81
Junyi Fan <junyi.fan@my.jcu.edu.au>
2026-06-27 10:33:01 +0800

    try to complete

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,22 @@
+"""
+CP1404/CP5632 Practical 05
+Word Occurrences Counter
+Estimate: 25 minutes
+Actual:
+"""
+
+def main():
+    text = input("Text: ")
+
+    words = text.split()
+
+    word_to_count = {}
+    for word in words:
+        word_to_count[word] = word_to_count.get(word, 0) + 1
+
+    unique_words = sorted(word_to_count.keys())
+
+    max_length = max(len(word) for word in unique_words) if unique_words else 0
+
+    for word in unique_words:
+        print(f"{word:{max_length}}")
