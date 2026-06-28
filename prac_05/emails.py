20033622095e1cee15aa57868d98bd682097a022
Junyi Fan <junyi.fan@my.jcu.edu.au>
2026-06-28 17:17:24 +0800

    completed extract_name_from_email(email) function

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -6,6 +6,7 @@ Actual:
 """
 
 def main():
+    """Enter user's name and confirm validation, output neat result"""
     email_to_name = {}
 
     email = input("Email: ").strip()
@@ -13,3 +14,10 @@ def main():
         suggested_name = extract_name_from_email(email)
 
 def extract_name_from_email(email):
+    """Extract and format a likely name from an email address."""
+    prefix = email.split("@")[0]
+    name_parts = prefix.repalce('.', ' ').replace('_', ' ').split()
+
+    extracted_name = " ".join(name_parts).title()
+
+    return extracted_name
