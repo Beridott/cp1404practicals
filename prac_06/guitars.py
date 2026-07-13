84729d861063df600627927a7ce5d9964508123c
Junyi Fan <junyi.fan@my.jcu.edu.au>
2026-07-13 10:20:58 +0800

    def main()

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -4,4 +4,9 @@ Client program to collect user's guitars and display them in a formatted list.
 Estimate time: 40
 Actual time:
 """
+from guitar import Guitar
+def main():
+    """Collect guitar data and display a formatted list."""
+    guitars = []
 
+    print("My guitars!")
