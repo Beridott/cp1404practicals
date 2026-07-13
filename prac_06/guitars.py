4bb9634d1c3b6a967031c0f8b806a351ea2e6c44
Junyi Fan <junyi.fan@my.jcu.edu.au>
2026-07-13 19:03:28 +0800

    Add guitar list display loop with formatting

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -28,3 +28,15 @@ def main():
             print("Invalide input. Please enter valid number for year and cost.")
 
         name = input("Name: ")
+
+        if guitars:
+            print("\nThese are my guitars:")
+            for i, guitar in enumerate(guitars, 1):
+                vintage_string = " (vintage)" if guitar.is_vintage() else ""
+
+                print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+        else:
+            print("\nNo guitars entered.")
+
+    if __name__ == '__main__':
+        main()
