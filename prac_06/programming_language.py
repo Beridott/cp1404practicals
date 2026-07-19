34de39cf0f7ed71da96b62f7169fae50434ffd9f
Junyi Fan <junyi.fan@my.jcu.edu.au>
2026-07-19 16:36:17 +0800

    completed def __init__

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,5 +1,16 @@
 """
 Programming Language Class.
 Estimate: 15 minutes
-Actual:   10 minutes
+Actual: 12 minutes
 """
+
+
+class ProgrammingLanguage:
+    """Represent a programming language object."""
+
+    def __init__(self, name="", typing="", reflection=False, year=0):
+        """Initialise a ProgramingLanguage instance."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
