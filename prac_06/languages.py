f79bec436627c06e97ce36d716ec8aa521b863af
Junyi Fan <junyi.fan@my.jcu.edu.au>
2026-07-19 17:28:21 +0800

    completed languages.py

---
+++ prac_06/languages.py
@@ -0,0 +1,25 @@
+"""
+CP1404/CP5632 Practical
+Programming Language Client Program
+Estimate: 15 minutes
+Actual: 13
+"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+def main():
+    python = ProgrammingLanguage("Python", "dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    print(python)
+
+    languages = [python, ruby, visual_basic]
+
+    print("\nThe dynamically typed languages are")
+    for language in languages:
+        if language.is_dynamic():
+            print(;anguage.name)
+
+if __name__ == '__main__':
+    main()
